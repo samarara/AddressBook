4e17f2a121ab92ce51916eb91a3d8b3719317178
samarara <samarara@SAMARARA-MAC>
2016-08-12 15:36:13 -0400

    Add project files.

---
+++ AddressBook/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace AddressBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStreetNo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtStreetName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtProvince = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cmdInsert = new System.Windows.Forms.Button();
+            this.cmdUpdate = new System.Windows.Forms.Button();
+            this.cmdDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(34, 52);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(208, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Street No.";
+            // 
+            // txtStreetNo
+            // 
+            this.txtStreetNo.Location = new System.Drawing.Point(34, 125);
+            this.txtStreetNo.Name = "txtStreetNo";
+            this.txtStreetNo.Size = new System.Drawing.Size(203, 20);
+            this.txtStreetNo.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Street Name";
+            // 
+            // txtStreetName
+            // 
+            this.txtStreetName.Location = new System.Drawing.Point(34, 201);
+            this.txtStreetName.Name = "txtStreetName";
+            this.txtStreetName.Size = new System.Drawing.Size(200, 20);
+            this.txtStreetName.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "City";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(34, 273);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(205, 20);
+            this.txtCity.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 323);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Province";
+            // 
+            // txtProvince
+            // 
+            this.txtProvince.Location = new System.Drawing.Point(34, 359);
+            this.txtProvince.Name = "txtProvince";
+            this.txtProvince.Size = new System.Drawing.Size(211, 20);
+            this.txtProvince.TabIndex = 9;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(302, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(543, 355);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // cmdInsert
+            // 
+            this.cmdInsert.Location = new System.Drawing.Point(34, 426);
+            this.cmdInsert.Name = "cmdInsert";
+            this.cmdInsert.Size = new System.Drawing.Size(211, 35);
+            this.cmdInsert.TabIndex = 11;
+            this.cmdInsert.Text = "Insert Record";
+            this.cmdInsert.UseVisualStyleBackColor = true;
+            this.cmdInsert.Click += new System.EventHandler(this.cmdInsert_Click);
+            // 
+            // cmdUpdate
+            // 
+            this.cmdUpdate.Enabled = false;
+            this.cmdUpdate.Location = new System.Drawing.Point(334, 426);
+            this.cmdUpdate.Name = "cmdUpdate";
+            this.cmdUpdate.Size = new System.Drawing.Size(211, 35);
+            this.cmdUpdate.TabIndex = 12;
+            this.cmdUpdate.Text = "Update Record";
+            this.cmdUpdate.UseVisualStyleBackColor = true;
+            this.cmdUpdate.Click += new System.EventHandler(this.cmdUpdate_Click);
+            // 
+            // cmdDelete
+            // 
+            this.cmdDelete.Enabled = false;
+            this.cmdDelete.Location = new System.Drawing.Point(634, 426);
+            this.cmdDelete.Name = "cmdDelete";
+            this.cmdDelete.Size = new System.Drawing.Size(211, 35);
+            this.cmdDelete.TabIndex = 13;
+            this.cmdDelete.Text = "Delete Record";
+            this.cmdDelete.UseVisualStyleBackColor = true;
+            this.cmdDelete.Click += new System.EventHandler(this.cmdDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 486);
+            this.Controls.Add(this.cmdDelete);
+            this.Controls.Add(this.cmdUpdate);
+            this.Controls.Add(this.cmdInsert);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtProvince);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtStreetName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtStreetNo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "AddressBook";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtStreetNo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtStreetName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtProvince;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button cmdInsert;
+        private System.Windows.Forms.Button cmdUpdate;
+        private System.Windows.Forms.Button cmdDelete;
+    }
+}
+
---
+++ AddressBook/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AddressBook
+{
+    public partial class Form1 : Form
+    {
+        //global variables
+        private string path = "AddressBook.txt";
+        private int index = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            readFile();
+        }
+
+        private void cmdInsert_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(path, true);
+            sw.WriteLine(txtName.Text + "," + txtStreetNo.Text + "," + txtStreetName.Text + "," + txtCity.Text + "," + txtProvince.Text);
+            sw.Close();
+            listBox1.Items.Clear();
+            readFile();
+
+            //clear text field and set focue back on the first text box
+            clearTextSetFocus();
+
+        }
+
+        private void cmdUpdate_Click(object sender, EventArgs e)
+        {
+            //delete existing file
+            File.Delete(path);
+            StreamWriter sw = new StreamWriter(path);
+            for(int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (i == listBox1.SelectedIndex)
+                {
+                    sw.WriteLine(txtName.Text + "," + txtStreetNo.Text + "," + txtStreetName.Text + "," + txtCity.Text + "," + txtProvince.Text);
+                }
+                else
+                {
+                    sw.WriteLine(listBox1.Items[i].ToString());
+                }
+            }
+            sw.Close();
+            listBox1.Items.Clear();
+            readFile();
+            clearTextSetFocus();
+            setControlState("i");
+            
+        }
+
+        private void cmdDelete_Click(object sender, EventArgs e)
+        {
+            File.Delete(path);
+            StreamWriter sw = new StreamWriter(path);
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (i != listBox1.SelectedIndex)
+                {
+                    sw.WriteLine(listBox1.Items[i].ToString());
+                }
+                
+            }
+            sw.Close();
+            listBox1.Items.Clear();
+            readFile();
+            clearTextSetFocus();
+            setControlState("i");
+        }
+
+        private void readFile()
+        {
+            StreamReader sr = new StreamReader(path);
+            string record = sr.ReadLine();
+            while (record != null)
+            {
+                //display the code in listbox
+                listBox1.Items.Add(record);
+                record = sr.ReadLine();
+            }
+            sr.Close();
+        }
+
+        private void clearTextSetFocus()
+        {
+            txtName.Clear();
+            txtStreetNo.Clear();
+            txtStreetName.Clear();
+            txtCity.Clear();
+            txtProvince.Clear();
+
+            txtName.Focus();
+        }
+
+        private void setControlState(string state)
+        {
+            if (state.Equals("u/d"))
+            {
+                cmdInsert.Enabled = false;
+                cmdUpdate.Enabled = true;
+                cmdDelete.Enabled = true;
+            }
+            else if (state.Equals("i"))
+            {
+                cmdInsert.Enabled = true;
+                cmdUpdate.Enabled = false;
+                cmdDelete.Enabled = false;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            index = listBox1.SelectedIndex;
+            if (index > -1)
+            {
+                char[] delim = { ',' };
+                string[] record = listBox1.SelectedItem.ToString().Split(delim);
+                txtName.Text = record[0];
+                txtStreetNo.Text = record[1];
+                txtStreetName.Text = record[2];
+                txtCity.Text = record[3];
+                txtProvince.Text = record[4];
+                setControlState("u/d");
+            }
+           
+        }
+    }
+}
